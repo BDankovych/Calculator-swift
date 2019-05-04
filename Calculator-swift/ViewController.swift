421455729a5ce4bab83d02c85adbb93f39b8ea0d
Bohdan Dankovych <bohdan.dankovych@gmail.com>
2019-05-04 12:36:54 +0300

    initial commit

---
+++ Calculator-swift/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Calculator-swift
+//
+//  Created by Bohdan Dankovych on 5/4/19.
+//  Copyright © 2019 Bohdan Dankovych. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
